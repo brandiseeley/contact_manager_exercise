a783764e26e553cc13f1cd7ae3b08478572a2871
Brandi Seeley <brandi.n.seeley@gmail.com>
2024-04-11 12:58:06 +0800

    Skeleton functionality for adding contacts.

---
+++ public/javascripts/contact_manager.js
@@ -0,0 +1,29 @@
+let ContactManager = (function() {
+  const DOMAIN = 'http://localhost:3000'
+  // Private
+
+  // Public
+  // Returns promises?
+  return {
+    async allContacts() {
+      let response = await fetch('http://localhost:3000/api/contacts');
+      let json = await response.json();
+      return json;
+    },
+
+    async addContact(data) {
+      let requestObject = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      };
+      let response = await fetch(DOMAIN + '/api/contacts/', requestObject);
+      let json = await response.json();
+      return json;
+    }
+  };
+})();
+
+export { ContactManager };
---
+++ public/javascripts/main.js
@@ -0,0 +1,85 @@
+import { ContactManager } from './contact_manager.js';
+
+const Templater = (function() {
+  let contactTemplate;
+
+  function contact(object) {
+    if (contactTemplate) return contactTemplate(object);
+
+    let html = document.querySelector('#contactTemplate').innerHTML;
+    contactTemplate = Handlebars.compile(html);
+    return contactTemplate(object);
+  }
+
+  return {
+    contact,
+  };
+})();
+
+// Orchestrates between Contact Manager and Templater
+const DOM = (function() {
+  // TODO: Badly named. renderContacts fetches and renders
+  //       renderContact takes existing data.
+  //       Fetch data in main and send it to renderContacts.
+  //       DOM shouldn't be async
+  async function renderContacts() {
+    let contacts = await ContactManager.allContacts();
+
+    // TODO: Use handlebars partial instead of looping through contacts.
+    let contactDiv = document.querySelector('#contacts');
+    contacts.forEach(contact => {
+      let html = Templater.contact(contact);
+      contactDiv.insertAdjacentHTML('beforeend', html);
+    });
+  }
+
+  // TODO: Should we add to the end, or rerender
+  //       to properly sort contacts?
+  function renderContact(contact) {
+    let html = Templater.contact(contact);
+    let contactDiv = document.querySelector('#contacts');
+    contactDiv.insertAdjacentHTML('beforeend', html);
+  }
+
+  function parseForm(form) {
+    let object = {};
+
+    for (let element of form.elements) {
+      console.log(element);
+      if (element.type !== 'submit' && element.type !== 'reset') {
+        object[element.name] = element.value;
+      }
+    }
+    return object;
+  }
+
+  return {
+    renderContacts,
+    renderContact,
+    parseForm,
+  };
+})();
+
+const Handlers = (function() {
+  async function newContact(event) {
+    event.preventDefault();
+    let form = document.querySelector('#contactForm');
+    let newContactData = DOM.parseForm(form);
+    console.log(newContactData);
+    let addedContactData = await ContactManager.addContact(newContactData);
+    DOM.renderContact(addedContactData);
+  }
+
+  return {
+    newContact,
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+  let newContactForm = document.querySelector('#contactForm');
+
+  DOM.renderContacts();
+
+  newContactForm.addEventListener('submit', Handlers.newContact);
+
+});
