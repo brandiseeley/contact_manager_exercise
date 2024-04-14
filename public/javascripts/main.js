a49b910eb91a8558da09b25fc9ded8a3dee2adf3
Brandi Seeley <brandi.n.seeley@gmail.com>
2024-04-14 13:58:49 +0800

    Extract FormManager to a module

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,83 +5,7 @@
 import { ContactManager } from '../modules/contact_manager.js';
 import { Templater } from '../modules/template_manager.js';
 import { Utility } from '../modules/utilities.js';
-
-const Validator = (function() {
-  let nameField;
-  let emailField;
-  let phoneField;
-  let fields;
-  let flashMessageContainer;
-  let errorMessage;
-  let successMessage;
-
-  function validInputs() {
-    for (let field of fields) {
-      if (!field.validity.valid) return false;
-    }
-    return true;
-  }
-
-  function showErrors() {
-    for (let field of fields) {
-      if (!field.validity.valid) {
-        let error = field.nextElementSibling;
-        error.textContent = field.validationMessage;
-      }
-    }
-  }
-
-  function suppressError(input) {
-    let error = input.nextElementSibling;
-    error.textContent = '';
-  }
-
-  function clearAllErrors() {
-    for (let field of fields) {
-      let error = field.nextElementSibling;
-      error.textContent = '';
-    }
-  }
-
-  function flashMessage(responseData) {
-    flashMessageContainer.classList.remove('hidden');
-    if (responseData.success) {
-      successMessage.textContent = responseData.message;
-    } else {
-      errorMessage.textContent = responseData.message;
-    }
-
-    setTimeout(clearFlashMessage, 3000);
-  }
-
-  function clearFlashMessage() {
-    flashMessageContainer.classList.add('hidden');
-    setTimeout(() => {
-      successMessage.textContent = '';
-      errorMessage.textContent = '';
-    }, 750);
-  }
-
-  function init() {
-    flashMessageContainer = document.querySelector('#flashMessage');
-    successMessage = document.querySelector('#successMessage');
-    errorMessage = document.querySelector('#errorMessage');
-    nameField = document.querySelector('#contactForm input[name="full_name"]');
-    emailField = document.querySelector('#contactForm input[name="email"]');
-    phoneField = document.querySelector('#contactForm input[name="phone_number"]');
-    fields = [nameField, emailField, phoneField];
-  }
-
-  return {
-    validInputs,
-    showErrors,
-    suppressError,
-    clearAllErrors,
-    flashMessage,
-    clearFlashMessage,
-    init,
-  };
-})();
+import { Validator, FormManager  } from '../modules/form_manager.js';
 
 const TagManager = (function() {
   let filteringTags;
@@ -112,93 +36,7 @@ const TagManager = (function() {
   };
 })();
 
-const FormManager = (function() {
-  let form;
-  let formWrapper;
-  let formHeader;
-
-  let hide = () => formWrapper.classList.add('hidden');
-  let show = () => formWrapper.classList.remove('hidden');
-  let isHidden = () => formWrapper.classList.contains('hidden');
-  let setHeader = (title) => { formHeader.textContent = title };
-  let setEditId = (value) => document.querySelector('#editId').setAttribute('value', value);
-
-  function populateInputs(contactDiv) {
-    let name = contactDiv.querySelector('.full_name').textContent;
-    let phone = contactDiv.querySelector('span.phone_number').textContent;
-    let email = contactDiv.querySelector('span.email').textContent;
-    let tagElements = Array.from(contactDiv.querySelectorAll('div.tags .tag'));
-    let tags = tagElements.map(tag => tag.textContent).join(', ');
-    let id = contactDiv.dataset.id;
-    form.querySelector('.full_name').value = name;
-    form.querySelector('.phone_number').value = phone;
-    form.querySelector('.email').value = email;
-    form.querySelector('.tags').value = tags;
-    setEditId(id);
-  }
-
-  function showContactForm(title, contactDiv) {
-    Utility.scrollToTop();
-    let delayToShow = 0;
-    if (!isHidden()) {
-      hideContactForm();
-      delayToShow = 750;
-    }
-    setTimeout(() => {
-      setHeader(title);
-      if (contactDiv) populateInputs(contactDiv);
-      show();
-    }, delayToShow);
-  }
-
-  let showEditContactForm = (contactDiv) => { showContactForm('Edit Contact', contactDiv) };
-  let showAddContactForm = () => { showContactForm('Add Contact') };
-
-  function hideContactForm() {
-    Utility.scrollToTop();
-    setEditId('');
-    hide();
-    setTimeout(() => {
-      Validator.clearAllErrors();
-      formHeader.textContent = '';
-      formWrapper.querySelector('form').reset();
-    }, 750);
-  }
-
-  function parseForm() {
-    let contactData = {};
-
-    for (let element of form.elements) {
-      if (element.type !== 'submit' && element.type !== 'reset') {
-        contactData[element.name] = element.value;
-      }
-    }
-    contactData.tags = contactData.tags.split(',')
-                                       .map(str => str.trim())
-                                       .filter(str => str)
-                                       .join(',');
-    return contactData;
-  }
-
-  function init() {
-    form = document.querySelector('#contactForm');
-    formWrapper = document.querySelector('#contactFormWrapper');
-    formHeader = document.querySelector('h2.contactForm');
-  }
-
-  return {
-    showEditContactForm,
-    showAddContactForm,
-    hideContactForm,
-    parseForm,
-    init,
-  };
-})();
-
 const Manager = (function() {
-  function contactId(contactDiv) {
-    return contactDiv.dataset.id;
-  }
 
   async function renderContacts(contacts, query) {
     if (contacts === undefined) {
@@ -238,7 +76,7 @@ const Manager = (function() {
         !(event.target.classList.contains('edit') ||
         event.target.classList.contains('delete'))) return;
     let contactDiv = event.target.closest('div');
-    let id = contactId(contactDiv);
+    let id = FormManager.contactId(contactDiv);
     if (event.target.className === 'delete') {
       let responseData = await ContactManager.deleteContact(id);
       Validator.flashMessage(responseData);
@@ -254,7 +92,7 @@ const Manager = (function() {
     FormManager.hideContactForm();
   }
 
-  function addTag(event) {
+  function tagClick(event) {
     if (event.target.tagName === 'BUTTON' && event.target.className === 'tag') {
       TagManager.addTag(event.target.textContent.trim());
       Utility.scrollToTop();
@@ -286,7 +124,7 @@ const Manager = (function() {
     addOrEdit,
     editOrDelete,
     cancelAddOrEdit,
-    addTag,
+    tagClick,
     filterByName,
     focusOnInput,
     removeTag,
@@ -313,6 +151,6 @@ document.addEventListener('DOMContentLoaded', () => {
   cancelButton.addEventListener('click', Manager.cancelAddOrEdit);
   searchForm.addEventListener('keyup', Manager.filterByName);
   contacts.addEventListener('click', Manager.editOrDelete);
-  contacts.addEventListener('click', Manager.addTag);
+  contacts.addEventListener('click', Manager.tagClick);
   filteringTags.addEventListener('click', Manager.removeTag);
 });
---
+++ public/modules/form_manager.js
@@ -0,0 +1,166 @@
+/* eslint-disable max-statements */
+import { Utility } from '../modules/utilities.js';
+
+const Validator = (function() {
+  let nameField;
+  let emailField;
+  let phoneField;
+  let fields;
+  let flashMessageContainer;
+  let errorMessage;
+  let successMessage;
+
+  function validInputs() {
+    for (let field of fields) {
+      if (!field.validity.valid) return false;
+    }
+    return true;
+  }
+
+  function showErrors() {
+    for (let field of fields) {
+      if (!field.validity.valid) {
+        let error = field.nextElementSibling;
+        error.textContent = field.validationMessage;
+      }
+    }
+  }
+
+  function suppressError(input) {
+    let error = input.nextElementSibling;
+    error.textContent = '';
+  }
+
+  function clearAllErrors() {
+    for (let field of fields) {
+      let error = field.nextElementSibling;
+      error.textContent = '';
+    }
+  }
+
+  function flashMessage(responseData) {
+    flashMessageContainer.classList.remove('hidden');
+    if (responseData.success) {
+      successMessage.textContent = responseData.message;
+    } else {
+      errorMessage.textContent = responseData.message;
+    }
+
+    setTimeout(clearFlashMessage, 3000);
+  }
+
+  function clearFlashMessage() {
+    flashMessageContainer.classList.add('hidden');
+    setTimeout(() => {
+      successMessage.textContent = '';
+      errorMessage.textContent = '';
+    }, 750);
+  }
+
+  function init() {
+    flashMessageContainer = document.querySelector('#flashMessage');
+    successMessage = document.querySelector('#successMessage');
+    errorMessage = document.querySelector('#errorMessage');
+    nameField = document.querySelector('#contactForm input[name="full_name"]');
+    emailField = document.querySelector('#contactForm input[name="email"]');
+    phoneField = document.querySelector('#contactForm input[name="phone_number"]');
+    fields = [nameField, emailField, phoneField];
+  }
+
+  return {
+    validInputs,
+    showErrors,
+    suppressError,
+    clearAllErrors,
+    flashMessage,
+    clearFlashMessage,
+    init,
+  };
+})();
+
+const FormManager = (function() {
+  let form;
+  let formWrapper;
+  let formHeader;
+
+  let hide = () => formWrapper.classList.add('hidden');
+  let show = () => formWrapper.classList.remove('hidden');
+  let isHidden = () => formWrapper.classList.contains('hidden');
+  let setHeader = (title) => { formHeader.textContent = title };
+  let setEditId = (value) => document.querySelector('#editId').setAttribute('value', value);
+  let contactId = (contactDiv) => contactDiv.dataset.id;
+
+  function populateInputs(contactDiv) {
+    let name = contactDiv.querySelector('.full_name').textContent;
+    let phone = contactDiv.querySelector('span.phone_number').textContent;
+    let email = contactDiv.querySelector('span.email').textContent;
+    let tagElements = Array.from(contactDiv.querySelectorAll('div.tags .tag'));
+    let tags = tagElements.map(tag => tag.textContent).join(', ');
+    let id = contactDiv.dataset.id;
+    form.querySelector('.full_name').value = name;
+    form.querySelector('.phone_number').value = phone;
+    form.querySelector('.email').value = email;
+    form.querySelector('.tags').value = tags;
+    setEditId(id);
+  }
+
+  function showContactForm(title, contactDiv) {
+    Utility.scrollToTop();
+    let delayToShow = 0;
+    if (!isHidden()) {
+      hideContactForm();
+      delayToShow = 750;
+    }
+    setTimeout(() => {
+      setHeader(title);
+      if (contactDiv) populateInputs(contactDiv);
+      show();
+    }, delayToShow);
+  }
+
+  let showEditContactForm = (contactDiv) => { showContactForm('Edit Contact', contactDiv) };
+  let showAddContactForm = () => { showContactForm('Add Contact') };
+
+  function hideContactForm() {
+    Utility.scrollToTop();
+    setEditId('');
+    hide();
+    setTimeout(() => {
+      Validator.clearAllErrors();
+      formHeader.textContent = '';
+      formWrapper.querySelector('form').reset();
+    }, 750);
+  }
+
+  function parseForm() {
+    let contactData = {};
+
+    for (let element of form.elements) {
+      if (element.type !== 'submit' && element.type !== 'reset') {
+        contactData[element.name] = element.value;
+      }
+    }
+    contactData.tags = contactData.tags.split(',')
+                                       .map(str => str.trim())
+                                       .filter(str => str)
+                                       .join(',');
+    return contactData;
+  }
+
+  function init() {
+    form = document.querySelector('#contactForm');
+    formWrapper = document.querySelector('#contactFormWrapper');
+    formHeader = document.querySelector('h2.contactForm');
+  }
+
+  return {
+    showEditContactForm,
+    showAddContactForm,
+    hideContactForm,
+    contactId,
+    parseForm,
+    init,
+  };
+})();
+
+export { Validator, FormManager };
